7b2a4ccca3bdfe737b149df6648d8923d5503950
tim <timothyfdavison@gmail.com>
2025-03-22 22:28:50 -0400

    Beginning to update readme. Taking port out of the react base URL for conference-mapper.com

--- react-frontend/src/config/leafletConfig.js
+++ react-frontend/src/config/leafletConfig.js
@@ -2,11 +2,13 @@ import L from 'leaflet';
 
 // Set leaflet to use locally stored icons
 const markerIconUrl = '/leaflet-icons/marker-icon.png';
+const markerIcon2xUrl = '/leaflet-icons/marker-icon-2x.png';
 const markerShadowUrl = '/leaflet-icons/marker-shadow.png';
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
   iconUrl: markerIconUrl,
+  iconRetinaUrl: markerIcon2xUrl,
   shadowUrl: markerShadowUrl,
 });
 
